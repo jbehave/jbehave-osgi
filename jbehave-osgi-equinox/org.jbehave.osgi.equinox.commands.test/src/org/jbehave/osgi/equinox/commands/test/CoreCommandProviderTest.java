41535152aa229b711e59440206c1a5f19f1bae87
Cristiano Gavião <cvgaviao@gmail.com>
2011-06-30 17:56:51 -0300

    renamed equinox directories.

--- jbehave-osgi-equinox/org.jbehave.osgi.equinox.commands.test/src/org/jbehave/osgi/equinox/commands/test/CoreCommandProviderTest.java
+++ jbehave-osgi-equinox/org.jbehave.osgi.equinox.commands.test/src/org/jbehave/osgi/equinox/commands/test/CoreCommandProviderTest.java
@@ -1,20 +1,18 @@
 package org.jbehave.osgi.equinox.commands.test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.apache.aries.blueprint.annotation.Bean;
-import org.apache.aries.blueprint.annotation.Inject;
 import org.jbehave.osgi.equinox.commands.CoreCommandProvider;
 import org.junit.Test;
 import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
 
-@Bean(id="CoreCommandProviderTest")
 public class CoreCommandProviderTest {
 
-	@Inject(ref="commandProviderImpl")
 	private CoreCommandProvider commandProvider;
 
 	@Test
@@ -23,31 +21,30 @@ public class CoreCommandProviderTest {
 		Bundle bundle = FrameworkUtil.getBundle(CoreCommandProvider.class);
 		assertThat("CoreCommand bundle is not installed", bundle,
 				is(notNullValue()));
+		
+		// get the context 
+		BundleContext bundleContext = bundle.getBundleContext();
+		assertThat("CoreCommand bundle context is null", bundle,
+				is(notNullValue()));
+		
+		ServiceReference<CoreCommandProvider> serviceReference = bundleContext.getServiceReference(CoreCommandProvider.class);
+		assertThat("ServiceReference is null", serviceReference,
+				is(notNullValue()));
 
+		commandProvider = bundleContext.getService(serviceReference);
+		
 		// asserts that test bundle is installed
 		assertThat("CoreCommand was not inject by Blueprint DI", commandProvider,
 				is(notNullValue()));
-
-		
+	
 	}
 
 	@Test
 	public void ensureJbehaveServiceIsInjected() {
-		// asserts that test bundle is resolved
-		assertThat("CoreCommand was not inject by Blueprint DI", commandProvider,
-				is(notNullValue()));
 
 		// assert that injected bundleContext is same that Framework one.
 		assertThat("EmbedderService was not injected by Blueprint DI",
 				commandProvider.getEmbedderService(), is(notNullValue()));
 
-	}
-
-	public CoreCommandProvider getCommandProvider() {
-		return commandProvider;
-	}
-
-	public void setCommandProvider(CoreCommandProvider commandProvider) {
-		this.commandProvider = commandProvider;
 	}
 }
