e5ea63130e125d55b818f96780cff512804e913b
Cristiano Gaviao <cvgaviao@gmail.com>
2013-05-23 15:32:13 -0300

    added jbehave-osgi swtbot extension

---
+++ jbehave-osgi-equinox/org.jbehave.osgi.equinox.swtbot/src/org/jbehave/osgi/equinox/swtbot/junit/SwtbotAnnotatedEmbedderRunner.java
@@ -0,0 +1,31 @@
+package org.jbehave.osgi.equinox.swtbot.junit;
+
+import org.eclipse.swtbot.swt.finder.junit.ScreenshotCaptureListener;
+import org.jbehave.core.junit.AnnotatedEmbedderRunner;
+import org.junit.runner.notification.RunListener;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.model.InitializationError;
+
+/**
+ * 
+ * @author Cristiano Gavião
+ *
+ */
+public class SwtbotAnnotatedEmbedderRunner extends AnnotatedEmbedderRunner {
+
+	public SwtbotAnnotatedEmbedderRunner(Class<?> testClass)
+			throws InitializationError {
+		super(testClass);
+		
+	}
+	public void run(RunNotifier notifier) {
+		RunListener failureSpy = new ScreenshotCaptureListener();
+		notifier.removeListener(failureSpy); // remove existing listeners that could be added by suite or class runners
+		notifier.addListener(failureSpy);
+		try {
+			super.run(notifier);
+		} finally {
+			notifier.removeListener(failureSpy);
+		}
+	}
+}
